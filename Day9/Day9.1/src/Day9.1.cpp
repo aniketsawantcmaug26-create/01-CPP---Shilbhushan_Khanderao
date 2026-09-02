4d034895e20c11ab487cb91ef58ec934fd263b5b
aniketsawantcmaug26-create <aniket.sawant.cmaug26@gmail.com>
2026-09-03 00:08:19 +0530

    Update Day9.1.cpp

--- Day9/Day9.1/src/Day9.1.cpp
+++ Day9/Day9.1/src/Day9.1.cpp
@@ -5,7 +5,7 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+// Rule of three 
 #include <iostream>
 using namespace std;
 
