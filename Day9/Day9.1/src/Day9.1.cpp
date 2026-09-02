ba4b64af9d4544f477e03f47889204b237c92f67
aniketsawantcmaug26-create <aniket.sawant.cmaug26@gmail.com>
2026-09-03 00:05:50 +0530

    Refactor copy assignment operator in Array class
    Refactor copy assignment operator and update comments.

--- Day9/Day9.1/src/Day9.1.cpp
+++ Day9/Day9.1/src/Day9.1.cpp
@@ -49,7 +49,7 @@ public:
 //		return *this;
 //	}
 
-	Array& operator=(const Array& a){
+	Array& operator=(const Array& a){          /// operatingg overload write we 
 		cout << "Copy Assingment Operator..." << endl;
 		if(this != &a){
 			delete[] data;  // to deleter current data first then point out to new memory location there store you data otherwise dangling pointer issue
@@ -79,7 +79,7 @@ int main() {
 
 	Array a2 = a;	// copy constructor
 
-	a1 = a;		// copy assignment operator=
+	a1 = a;		// copy assignment operator= or
 
 //	a1 = a1;
 
